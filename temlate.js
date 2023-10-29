55d329aa0aff104da004e63b95facdd012c55c43
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-10-29 18:21:06 +0200

    Update temlate.js

--- temlate.js
+++ temlate.js
@@ -1,3 +1,4 @@
+// розгалуження/умови
 const value = 5;
 
 if (value === 1) {
@@ -23,9 +24,11 @@ switch (value) {
   default:
     console.log("default");
 }
+
 const dictationResult = 2;
 const speakingTestResult = 5;
 let examResult = dictationResult + speakingTestResult;
+
 switch (true) {
   case examResult <= 6:
     console.log("bad result");
@@ -39,3 +42,50 @@ switch (true) {
   default:
     console.log("default");
 }
+
+// Області видимості
+
+let a = 15;
+
+if (true) {
+  a = 25;
+}
+console.log(a);
+
+let b = 15;
+
+if (true) {
+  let b = 1;
+  b = 25;
+}
+console.log(b);
+
+let c = 10;
+
+if (true) {
+  let c = 15;
+  if (true) {
+    c = 25;
+  }
+}
+console.log(c);
+
+let d = 10;
+
+if (true) {
+  let d = 15;
+  if (true) {
+    d = 25;
+  }
+  console.log(d);
+}
+
+let g = 10;
+
+if (true) {
+  g = 15;
+  if (true) {
+    g = 25;
+  }
+}
+console.log(g);
