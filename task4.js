450ca28b3c5776a5b029c2426300666927fc23ab
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-10-30 21:33:46 +0200

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,9 +1,9 @@
-const userInputValue = Number(
-  prompt(
-    "Enter a number. And we will check it.",
-    "write a number that is more than 0"
-  )
-);
+// const userInputValue = Number(
+//   prompt(
+//     "Enter a number. And we will check it.",
+//     "write a number that is more than 0"
+//   )
+// );
 
 // if (userInputValue % 2 === 0) {
 //   alert(`${userInputValue} is a even number.`);
@@ -17,6 +17,6 @@ const userInputValue = Number(
 //   alert(`${userInputValue} is a even number.`);
 // }
 
-userInputValue % 2
-  ? alert(`${userInputValue} is a odd number.`)
-  : alert(`${userInputValue} is a even number.`);
+// userInputValue % 2
+//   ? alert(`${userInputValue} is a odd number.`)
+//   : alert(`${userInputValue} is a even number.`);
