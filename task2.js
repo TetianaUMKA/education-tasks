d958c04369787888a6a470d3540e90f52c6ff846
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-10-30 16:53:17 +0200

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,13 @@
+let minutes = 0;
+let seconds = 0;
+
+minutes = 45;
+seconds = 6;
+
+if (seconds === 0) {
+  console.log(`${minutes} minutes`);
+} else {
+  console.log(`${minutes} minutes ${seconds} seconds`);
+}
+
+const str = "iterable element";
