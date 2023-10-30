1e98bb2d231dfaebad135128cf16aadd56151f9d
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-10-30 20:12:24 +0200

    Update task4.js

--- task4.js
+++ task4.js
@@ -5,8 +5,18 @@ const userInputValue = Number(
   )
 );
 
-if (userInputValue % 2 === 0) {
-  alert(`${userInputValue} is a even number.`);
-} else {
-  alert(`${userInputValue} is a odd number.`);
-}
+// if (userInputValue % 2 === 0) {
+//   alert(`${userInputValue} is a even number.`);
+// } else {
+//   alert(`${userInputValue} is a odd number.`);
+// }
+
+// if (userInputValue % 2) {
+//   alert(`${userInputValue} is a odd number.`);
+// } else {
+//   alert(`${userInputValue} is a even number.`);
+// }
+
+userInputValue % 2
+  ? alert(`${userInputValue} is a odd number.`)
+  : alert(`${userInputValue} is a even number.`);
