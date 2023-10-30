6866459f85a68bd55147539234c47547857a6785
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-10-30 19:27:57 +0200

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,17 +1,17 @@
-const value = prompt("What is the official name of JavaScript?", "write name");
-console.log(value);
+// const value = prompt("What is the official name of JavaScript?", "write name");
+// console.log(value);
 
-if (value !== "EcmaScript") {
-  alert("This is the wrong answer!🙅‍♀");
-  const value = prompt(
-    "Try again.You can do it. What is the official name of JavaScript?"
-  );
-  if (value !== "EcmaScript") {
-    console.log("wrong");
-    alert("This is the wrong answer!🙅‍♀ The right answer is EcmaScript.");
-  } else {
-    alert("Well done, this is the correct answer!🔥");
-  }
-} else {
-  alert("Well done, this is the correct answer!🔥");
-}
+// if (value !== "EcmaScript") {
+//   alert("This is the wrong answer!🙅‍♀");
+//   const value = prompt(
+//     "Try again.You can do it. What is the official name of JavaScript?"
+//   );
+//   if (value !== "EcmaScript") {
+//     console.log("wrong");
+//     alert("This is the wrong answer!🙅‍♀ The right answer is EcmaScript.");
+//   } else {
+//     alert("Well done, this is the correct answer!🔥");
+//   }
+// } else {
+//   alert("Well done, this is the correct answer!🔥");
+// }
