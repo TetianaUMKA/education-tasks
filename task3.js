a3d08bb24ec3ba9e97d008d1016dcc5cd7ccf17b
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-10-30 19:28:01 +0200

    Create task3.js

---
+++ task3.js
@@ -0,0 +1,26 @@
+const userInputValue = Number(prompt("Enter a number", "write a number"));
+console.log(typeof userInputValue);
+
+switch (true) {
+  case userInputValue > 0:
+    alert(`${userInputValue} is a positive value`);
+    break;
+  case userInputValue < 0:
+    alert(`${userInputValue} is a negatite value`);
+    break;
+  case userInputValue === 0:
+    alert(`${userInputValue} is a null`);
+    break;
+  default:
+    alert(`You enter no number`);
+}
+
+// if (userInputValue === 0) {
+//   alert(`${userInputValue} is a null`);
+// } else if (userInputValue > 0) {
+//   alert(`${userInputValue} is a positive value`);
+// } else if (userInputValue < 0) {
+//   alert(`${userInputValue} is a negatite value`);
+// } else {
+//   alert(`You enter no number`);
+// }
