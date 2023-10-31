2fcee1e6e272234c48be33d8fe61743ea8929238
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-10-31 22:46:05 +0200

    Update task7.js

--- task7.js
+++ task7.js
@@ -1,8 +1,11 @@
 let link = "https://my-site.com/about";
 
-if (!link.endsWith("/") && link.includes("my-site")) {
-  link += "/";
-}
+// if (!link.endsWith("/") && link.includes("my-site")) {
+//   link += "/";
+// }
+
+!link.endsWith("/") && link.includes("my-site") ? (link += "/") : (link += "");
+
 console.log(link);
 
 // let link = "https://someone.com/about";
