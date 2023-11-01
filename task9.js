692bc1af2364439a3ba076ae482bf011ebb0f8e3
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-11-01 13:48:54 +0200

    Create task9.js

---
+++ task9.js
@@ -0,0 +1,49 @@
+const dayUntilDeadlina = 5;
+
+// switch (true) {
+//   case dayUntilDeadlina === 0:
+//     console.log("Today");
+//     break;
+//   case dayUntilDeadlina === 1:
+//     console.log("Tomorrow");
+//     break;
+//   case dayUntilDeadlina === 2:
+//     console.log("The day after tomorrow");
+//     break;
+//   default:
+//     console.log(" A date in the future");
+// }
+
+// if (dayUntilDeadlina === 0) {
+//   console.log("Today");
+// } else if (dayUntilDeadlina === 1) {
+//   console.log("Tomorrow");
+// } else if (dayUntilDeadlina === 2) {
+//   console.log("The day after tomorrow");
+// } else {
+//   console.log("A date in the future");
+// }
+
+// if (!dayUntilDeadlina) {
+//   console.log("Today");
+// } else if (dayUntilDeadlina === 1) {
+//   console.log("Tomorrow");
+// } else if (dayUntilDeadlina === 2) {
+//   console.log("The day after tomorrow");
+// } else {
+//   console.log("A date in the future");
+// }
+
+switch (dayUntilDeadlina) {
+  case 0:
+    console.log("Today");
+    break;
+  case 1:
+    console.log("Tomorrow");
+    break;
+  case 2:
+    console.log("The day after tomorrow");
+    break;
+  default:
+    console.log(" A date in the future");
+}
