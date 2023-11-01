bcae88e0dd86a2818b67f0a14d39d0bb6ae5d5ee
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-11-01 18:05:55 +0200

    Update task11.js

--- task11.js
+++ task11.js
@@ -1,4 +1,4 @@
-const userLogin = prompt("Enter your name, please.");
+// const userLogin = prompt("Enter your name, please.");
 
 // if (!userLogin) {
 //   console.log("Cancelled");
@@ -13,11 +13,11 @@ const userLogin = prompt("Enter your name, please.");
 //   alert("User wiht this name does not exist");
 // }
 
-if (!userLogin) {
-  console.log("Cancelled");
-} else if (userLogin === "Admin") {
-  const userPassword = prompt("Enter your password, please.");
-  userPassword ? alert("Hello👋, Admin!") : alert("This password is wrong!🔴");
-} else {
-  alert("User wiht this name does not exist");
-}
+// if (!userLogin) {
+//   console.log("Cancelled");
+// } else if (userLogin === "Admin") {
+//   const userPassword = prompt("Enter your password, please.");
+//   userPassword ? alert("Hello👋, Admin!") : alert("This password is wrong!🔴");
+// } else {
+//   alert("User wiht this name does not exist");
+// }
