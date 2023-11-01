439ce09f6357b6558adc5a7f6bc1bbb4133bab97
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-11-01 12:23:43 +0200

    Create task8.js

---
+++ task8.js
@@ -0,0 +1,28 @@
+let hours = 20;
+
+// if (hours < 17) {
+//   console.log("Pending");
+// } else if (hours >= 17 && hours <= 24) {
+//   console.log("Expires");
+// } else {
+//   console.log("Overdue");
+// }
+
+if (hours < 17) {
+  console.log("Pending");
+} else if (hours <= 24) {
+  console.log("Expires");
+} else {
+  console.log("Overdue");
+}
+
+// switch (true) {
+//   case hours < 17:
+//     console.log("Pending");
+//     break;
+//   case hours <= 24:
+//     console.log("Expires");
+//     break;
+//   default:
+//     console.log("Overdue");
+// }
