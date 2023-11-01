2f2c8126b1ee7e9eb5465bdc453a884e391dceca
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-11-01 14:28:10 +0200

    Create task10.js

---
+++ task10.js
@@ -0,0 +1,14 @@
+const min = 20;
+const max = 100;
+
+// for (let i = min; i <= max; i += 1) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+for (let i = min; i <= max; i += 1) {
+  if (!(i % 5)) {
+    console.log(i);
+  }
+}
