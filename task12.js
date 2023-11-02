453c2f1b448e93b17ac231c4e92beefa5d3795d7
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-11-02 23:02:43 +0200

    Create task12.js

---
+++ task12.js
@@ -0,0 +1,12 @@
+let weight = "88,3";
+let height = "1.75";
+weight = Number(weight.replace(",", "."));
+height = Number(height);
+// height = +height;   this expression results in type number
+// console.log(typeof height);
+
+const bmi = (weight / Math.pow(height, 2)).toFixed(1);
+
+// const bmi = Number((weight / height ** 2).toFixed(1));
+
+console.log(bmi);
