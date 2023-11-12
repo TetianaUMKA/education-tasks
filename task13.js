bf2de6b3c65a10cce825096c890694ecc26954f1
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-11-12 20:00:56 +0200

    Update task13.js

--- task13.js
+++ task13.js
@@ -4,15 +4,24 @@ const carsArray = [
   "red",
   "yellow",
   "brown",
+  "red",
   "white",
   "black",
   "white",
+  "red",
 ];
 console.log(carsArray);
 for (let i = 0; i < carsArray.length; i += 1) {
   if (carsArray[i] === "white") {
     const deletedCars = carsArray.splice(i, 1);
     console.log(deletedCars);
+  }
+}
+console.log(carsArray);
+
+for (let i = 0; i < carsArray.length; i += 1) {
+  if (carsArray[i] === "red") {
+    carsArray[i] = "grey";
   }
 }
 console.log(carsArray);
