e4514743029005113e1cb9271115d0e0fe3c464d
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-11-18 18:17:50 +0200

    Update task15.js

--- task15.js
+++ task15.js
@@ -4,3 +4,7 @@ const first = Number(arr[0]);
 const last = Number(arr[arr.length - 1]);
 const result = first * last;
 console.log(result);
+
+// const arr = values.split(" ");
+// const result = Number(arr[0] * arr[arr.length - 1]);
+// console.log(result);
