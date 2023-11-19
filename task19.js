7c8f9ae88414c926764c6c512d0381966f2cfdbe
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-11-19 22:38:06 +0200

    Update task19.js

--- task19.js
+++ task19.js
@@ -1,3 +1,17 @@
+// task A
 const string = "Welcome to the future";
 const result = string.split("").reverse().join("");
 console.log(result);
+
+// task B
+// const langs = ["python", "javascript", "c++", "haskel", "php", "ruby"];
+const langs = ["cpython", "bjavascript", "ac++", "haskel", "php", "ruby"];
+// const [arr] = [];
+for (let i = 0; i < langs.length; i += 1) {
+  console.log(langs[i][0]); // the first letter of each array element
+  if (langs[0] > langs[i]) {
+    const langsResult = langs.splice(i, 1)[0];
+    langs.unshift(langsResult);
+  }
+}
+console.log(langs);
