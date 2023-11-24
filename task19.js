79a6a4bd7e749fcaa47465cf501b6cacc1ae4fcc
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-11-24 17:41:32 +0200

    Update task19.js

--- task19.js
+++ task19.js
@@ -4,37 +4,19 @@ const result = string.split("").reverse().join("");
 console.log(result);
 
 // task B
-// const langs = ["python", "javascript", "c++", "haskel", "php", "ruby"];
-const langs = ["javascript", "c++", "haskel", "python", "ruby"];
-// const langs = ["cpython", "bjavascript", "ac++", "haskel", "php", "ruby"];
-// const langs = ["c", "b", "a", "h", "j", "p", "r"];
-// const [arr] = [];
-for (let i = 0; i < langs.length; i += 1) {
-  console.log(langs[i][0]); // the first letter of each array element
-  if (langs[0] > langs[i]) {
-    const langsResult = langs.splice(i, 1)[0];
-    langs.unshift(langsResult);
-  }
-}
+const langs = ["c", "b", "a", "j", "s", "h", "r", "p"];
+console.log(langs.sort()); // It sorts and changes the original
 console.log(langs);
 
 // task C
 const numbers = [2, 17, -156, 94, 1, 43, 35];
 let min = numbers[0];
-let ordinalArr = [];
-for (let i = min; i < numbers.length; i += 1) {
-  if (numbers[i] < min) {
-    const numbersResult = numbers.splice(i, 1)[0];
-    ordinalArr.push(numbersResult);
+for (const value of numbers) {
+  if (value < min) {
+    min = value;
   }
 }
-console.log(ordinalArr);
-console.log(numbers);
+console.log(min);
 
-// let min = numbers[0];
-// for (const value of numbers) {
-//   if (value < min) {
-//     min = value;
-//   }
-// }
-// console.log(min);
+const sortedValues = numbers.toSorted((a, b) => a - b); // It returns new array
+console.log(sortedValues);
