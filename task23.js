1cac7632e5d682376935e0a4402c5d8e6852b312
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-12-02 20:28:50 +0200

    Update task23.js

--- task23.js
+++ task23.js
@@ -1,4 +1,4 @@
-//task 23.1
+// task 23.1
 function findLargestNumber(numbers) {
   let min = numbers[0];
   for (const num of numbers) {
@@ -11,3 +11,18 @@ function findLargestNumber(numbers) {
 
 console.log(findLargestNumber([2, 15, 230, 45, 6, 97]));
 console.log(findLargestNumber([56, 4, 98, 450, 8, 68]));
+
+// task 23.2
+function findSmallestNumber(numbers) {
+  numbers = [...arguments];
+  let min = numbers[0];
+  for (const num of numbers) {
+    if (min > num) {
+      min = num;
+    }
+  }
+  return min;
+}
+
+console.log(findSmallestNumber(2, 15, 230, 45, 6, 97));
+console.log(findSmallestNumber(56, 4, 98, 450, 8, 68));
