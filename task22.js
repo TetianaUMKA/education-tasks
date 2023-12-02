1db319d62d60ed0661842db532c1256d73e1c1e3
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-12-02 19:35:03 +0200

    Update task22.js

--- task22.js
+++ task22.js
@@ -1,3 +1,4 @@
+// task 22.1
 function logItems(params) {
   // for (let i = 0, n = 1; i < params.length; i += 1, n += 1) {
   //   console.log(`${n}. ${params[i]}`);
@@ -8,3 +9,16 @@ function logItems(params) {
 }
 
 logItems(["Lexus", "Toyota", "Range Rover", "Audi"]);
+
+// task 22.2
+function printContactInfo(names, phones) {
+  names = names.split(",");
+  phones = phones.split(",");
+  for (let i = 0, j = 0; i < names.length, j < phones.length; i += 1, j += 1) {
+    console.log(`${i + 1}. ${names[i]} 👉 ${phones[j]}`);
+  }
+}
+printContactInfo(
+  "Jacob,William,Solomon,Artemis",
+  "89001234567,89001121232,89005556345,89007865439"
+);
