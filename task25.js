2fdc35d6655f2ff47354ad7c6ad90d641c68c94f
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-12-03 16:47:19 +0200

    Create task25.js

---
+++ task25.js
@@ -0,0 +1,11 @@
+function formatTime(minutes) {
+  let hours = Math.floor(minutes / 60);
+  minutes = minutes % 60;
+  return `${hours.toString().padStart(2, 0)}:${minutes
+    .toString()
+    .padStart(2, 0)}`;
+}
+
+console.log(formatTime(70)); // '01:10'
+console.log(formatTime(450)); // '07:30'
+console.log(formatTime(1441)); // '24:01'
