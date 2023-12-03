bb3f95c8462c0e898c363ba435ae89977145735f
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-12-03 13:52:37 +0200

    Create task24.js

---
+++ task24.js
@@ -0,0 +1,14 @@
+function calcAverage() {
+  const arr = [...arguments];
+  let average = 0;
+  let num = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    num += arr[i];
+    average += 1;
+  }
+  return num / average;
+}
+
+console.log(calcAverage(1, 2, 3, 4)); // 2.5
+console.log(calcAverage(14, 8, 2)); // 8
+console.log(calcAverage(27, 43, 2, 8, 36)); // 23.2
