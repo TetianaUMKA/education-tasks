8dea7e9f808bf450ce54dfa28c833252cb1258cf
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-12-13 20:06:12 +0200

    Update task33.js

--- rest_spread_dectructuring/task33.js
+++ rest_spread_dectructuring/task33.js
@@ -3,14 +3,14 @@
  * @param {Object} companyName, bots: { repairBots, defenceBots }
  * @returns {String}
  */
-function getBotReport({ companyName, bots: { repairBots, defenceBots } }) {
+function getBotReport({ companyName, botStock: { repairBots, defenceBots } }) {
   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
 }
 
 console.log(
   getBotReport({
     companyName: "Cyberdyne Systems",
-    bots: {
+    botStock: {
       repairBots: 150,
       defenceBots: 50,
     },
