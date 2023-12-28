f3e2cc48a3f0fddcb714c739dfd99c7ed1374c73
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-12-28 21:41:05 +0200

    Update further_tasks2.js

--- further_lessons/further_tasks2.js
+++ further_lessons/further_tasks2.js
@@ -59,3 +59,56 @@ createProduct2(object, logProduct2);
 createProduct2(object, logTotalPrice2);
 
 createProduct2(object, doubleQuantity);
+
+// 3
+
+const TRANSACTION_LIMIT = 1000;
+
+const account = {
+  username: "Jacob",
+  balance: 400,
+  withdrow(amount, onSuccess, onError) {
+    if (amount > TRANSACTION_LIMIT || amount > this.balance) {
+      onError({
+        status: "Error❌",
+        message:
+          "The limit is exceeded or there are insufficient founds in your account!",
+      });
+    } else {
+      this.balance -= amount;
+      onSuccess({
+        status: "Success✅",
+        message: `The operation is successful! The balance in your account is ${this.balance}`,
+      });
+    }
+  },
+  deposit(amount, onSuccess, onError) {
+    if (amount > TRANSACTION_LIMIT || amount <= 0) {
+      onError({
+        status: "Error❌",
+        message: "The limit is exceeded or the sum is zero or less!",
+      });
+    } else {
+      this.balance += amount;
+      onSuccess({
+        status: "Success✅",
+        message: `The operation is successful! Your account has been topped up by UAN ${amount}. The balance in your account is ${this.balance}`,
+      });
+    }
+  },
+};
+
+function hadleSuccess({ status, message }) {
+  console.log(`${status}: ${message}`);
+}
+
+function handleError({ status, message }) {
+  console.log(`${status}: ${message}`);
+}
+
+account.withdrow(2000, hadleSuccess, handleError);
+account.withdrow(600, hadleSuccess, handleError);
+account.withdrow(300, hadleSuccess, handleError);
+account.deposit(3000, hadleSuccess, handleError);
+account.deposit(700, hadleSuccess, handleError);
+account.deposit(900, hadleSuccess, handleError);
